d02ce0a1b36ba4f78f2398ae97aabdafa7c78841
kyo-135 <rlarlgh1358@gmail.com>
2026-06-29 12:46:52 +0900

    pages/oil.py, pages/parking.py 생성,  requirements.txt 업데이트

---
+++ pages/oil.py
@@ -0,0 +1,26 @@
+import streamlit as st
+from streamlit_extras.specialized_inputs import search_input
+
+# 지도 레이아웃과 주유소 목록 레이아웃 나누기
+map_col, list_col = st.columns([3, 2])
+
+with map_col:
+    # 지도 레이아웃 작성
+    pass
+
+with list_col:
+    # 주유소 목록 레이아웃 작성
+    # 착한주유소 보기와 내 주유소 보기 체크박스 생성
+    checkbox_good, checkbox_my = st.columns([1, 1])
+    with checkbox_good:
+        is_good_oil = st.checkbox('착한주유소 보기')
+    with checkbox_my:
+        is_my_oil = st.checkbox('내 주유소 보기')
+
+    # 검색창 생성
+    search_q = search_input("Search", label_visibility="collapsed", key="ex_search")
+    if search_q:
+        # 검색 로직 구현
+        pass
+
+    # 주유소 목록 시각화
