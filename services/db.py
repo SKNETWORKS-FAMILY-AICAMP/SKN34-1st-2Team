46e315b4dac2fdd3643bbdfdb12fd11dbc16dbbf
HJK013 <kimhj9995@naver.com>
2026-06-30 18:58:37 +0900

    DB 연결

--- services/db.py
+++ services/db.py
@@ -1,10 +1,43 @@
-import config as cf
+import mysql.connector
+import os
+from dotenv import load_dotenv
 
-class Database:
-    def __init__(self):
-        self.conn = self.connect()
+load_dotenv()
 
-    def connect(self):
-        return cf.DB_HOST
+connection = mysql.connector.connect(
+    host = os.getenv("DB_HOST"),
+    user = os.getenv("DB_USER"),
+    password = os.getenv("DB_PASSWORD"),
+    database = os.getenv("DB_NAME")
+)
 
-db = Database()
+
+# import mysql.connector
+# import os
+# from dotenv import load_dotenv
+
+# load_dotenv()
+
+# class Database:
+#     def __init__(self):
+#         self.conn = self.connect()
+
+#     def connect(self):
+#         try:
+#             connection = mysql.connector.connect(
+#                 host = os.getenv("DB_HOST"),
+#                 user = os.getenv("DB_USER"),
+#                 password = os.getenv("DB_PASSWORD"),
+#                 database = os.getenv("DB_NAME")
+#             ) 
+#             if connection.is_connected():
+#                 print('db연결성공') # 추후삭제
+#                 return connection
+#         except mysql.connector.Error as err:
+#             print(f"DB 연결 실패: {err}")
+#             return None
+#     def close(self):
+#         if self.conn and self.conn.is_connected():
+#             self.conn.close()
+
+# db = Database()
